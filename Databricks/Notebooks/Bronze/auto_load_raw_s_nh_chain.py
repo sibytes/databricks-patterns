c956a5bfa58be7e520014e397ff7f5dda9461734
shaunryan <shaunryan>
2023-04-22 10:44:18 +0000

    header, footer, audit

--- Databricks/Notebooks/Bronze/auto_load_raw_s_nh_chain.py
+++ Databricks/Notebooks/Bronze/auto_load_raw_s_nh_chain.py
@@ -3,6 +3,11 @@
 
 # COMMAND ----------
 
+dbutils.widgets.text("process_id", "-1")
+dbutils.widgets.text("table", "customers")
+
+# COMMAND ----------
+
 from dbxconfig import (
   Config, Timeslice, StageType, Read, DeltaLake
 )
@@ -13,22 +18,34 @@ import os
 
 # COMMAND ----------
 
+param_process_id = int(dbutils.widgets.get("process_id"))
+param_table = dbutils.widgets.get("table")
+print(f"""
+  param_process_id: {param_process_id}
+  param_table: {param_table}
+""")
+
+# COMMAND ----------
+
 pattern = "auto_load_schema"
 workspace_path = "/Workspace/autobricks"
+repo_path = "/Workspace/Repos"
 config_path = "../Config"
-if os.path.exists(workspace_path):
+if os.path.exists(workspace_path) and not os.getcwd().startswith(repo_path):
   config_path = f"{workspace_path}/Config"
 
 config_path
+
 # COMMAND ----------
 
 timeslice = Timeslice(day="*", month="*", year="*")
 config = Config(config_path=config_path, pattern=pattern)
-table_mapping = config.get_table_mapping(timeslice=timeslice, stage=StageType.raw, table="customers")
+table_mapping = config.get_table_mapping(timeslice=timeslice, stage=StageType.raw, table=param_table)
 
 # COMMAND ----------
 
 def load(
+  process_id:int,
   source:Read,
   destination:DeltaLake,
   await_termination:bool = True
@@ -41,14 +58,18 @@ def load(
     .load(source.path)
   )
 
-  print(stream.columns)
+  src_cols = [c for c in stream.columns if not c.startswith("_")]
+  sys_cols = [c for c in stream.columns if c.startswith("_")]
+
+  columns = src_cols + sys_cols + [
+    f"cast({process_id} as long) as _process_id",
+    "current_timestamp() as _load_date",
+    "_metadata"
+  ]
+  print("\n".join(columns))
 
   stream_data:StreamingQuery = (stream
-    .select(
-      "*",
-      fn.current_timestamp().alias("_load_date"),
-      "_metadata"
-    )
+    .selectExpr(*columns)
     .writeStream
     .options(**destination.options)
     .trigger(availableNow=True)
@@ -75,6 +96,19 @@ def load_hf(
     "mergeSchema": True
   }
 
+  header_schema = ",".join([
+  "flag string",
+  "row_count long",
+  "period long",
+  "batch string"
+  ])
+
+  footer_schema = ",".join([
+    "flag string",
+    "name string",
+    "period long"
+  ])
+
   location = destination.location.replace(destination.table, table_hf)
   spark.sql(f"""
   CREATE TABLE IF NOT EXISTS `{destination.database}`.`{table_hf}`
@@ -88,18 +122,55 @@ def load_hf(
   """)
 
   columns = [
-    "flag",
-    "_corrupt_record as data",
+    f"to_json(_corrupt_record, '{header_schema}') as header",
+    "_corrupt_record as raw_header",
+    "_process_id",
     "_load_date",
     "_metadata"
   ]
   # https://docs.databricks.com/delta/delta-change-data-feed.html
-  stream_hf:StreamingQuery = (spark.readStream
+  stream_header:StreamingQuery = (spark.readStream
     .format("delta") 
     .option("readChangeFeed", "true")
     .table(f"`{destination.database}`.`{destination.table}`")
-    .where("_change_type = 'insert' and flag  IN ('H','F')")
+    .where("_change_type = 'insert' and flag = 'H'")
     .selectExpr(*columns)
+  )
+
+  columns = [
+    f"to_json(_corrupt_record, '{footer_schema}') as footer",
+    "_corrupt_record as raw_footer",
+    "_process_id as f_process_id",
+    "_metadata as f_metadata"
+  ]
+  stream_footer:StreamingQuery = (spark.readStream
+    .format("delta") 
+    .option("readChangeFeed", "true")
+    .table(f"`{destination.database}`.`{destination.table}`")
+    .where("_change_type = 'insert' and flag = 'F'")
+    .selectExpr(*columns)
+  )
+
+  columns = [
+    "header",
+    "raw_header",
+    "raw_footer",
+    "footer",
+    "_process_id",
+    "_load_date",
+    "_metadata"
+  ]
+  stream_joined = (stream_header
+    .join(
+      stream_footer, 
+      fn.expr("""
+        _process_id = f_process_id AND
+        _metadata.file_name = f_metadata.file_name
+      """))
+    .selectExpr(*columns)
+  )
+
+  stream_write = (stream_joined
     .writeStream
     .options(**options_hf)
     .trigger(availableNow=True)
@@ -107,13 +178,71 @@ def load_hf(
   )
   
   if await_termination:
-    stream_hf.awaitTermination()
+    stream_write.awaitTermination()
 
 # COMMAND ----------
 
-source = table_mapping.source["customer_details_1"]
+def load_audit(
+  source:Read,
+  destination:DeltaLake,
+  await_termination:bool = True
+):
+
+  table_audit = "etl_audit"
+  location = destination.location.replace(destination.table, table_audit)
+  spark.sql(f"""
+  CREATE TABLE IF NOT EXISTS `{destination.database}`.`{table_audit}`
+  USING DELTA
+  LOCATION '{location}'
+  TBLPROPERTIES (
+    delta.appendOnly = true,
+    delta.autoOptimize.autoCompact = true,
+    delta.autoOptimize.optimizeWrite = true
+  )
+  """)
+
+  to_table = f"`{destination.database}`.`headerfooter`"
+  df = spark.sql(f"""
+    SELECT
+      cast(count(*) as long) as total_count,
+      cast(sum(if(d._is_valid, 1, 0)) as long) as valid_count,
+      cast(sum(if(d._is_valid, 0, 1)) as long) as invalid_count,
+      hf.header.row_count as expected_row_count,
+      d._metadata.*
+    FROM `{destination.database}`.`{destination.table}` as d
+    JOIN {to_table} as hf
+      ON hf._process_id = d._process_id
+      AND hf._metadata.file_name = d._metadata.file_name
+    GROUP BY 
+      hf.header.row_count, 
+      d._metadata.*
+  """)
+
+  result = (df.write
+    .format("delta")
+    .mode("append")
+    .option("schemaMerge", True)
+    .saveAsTable(to_table)
+  )
+
+# COMMAND ----------
+
 raw = table_mapping.destination
-config.set_checkpoint(source=source, destination=raw)
+
+if isinstance(table_mapping.source, dict):
+  for _, source in table_mapping.source.items():
+    print(f"""setting checkpoint
+      load: {source.database}.{source.database} to {raw.database}.{raw.database}
+      path: {raw.checkpoint}
+    """)
+    config.set_checkpoint(source=source, destination=raw)
+else:
+  source = table_mapping.source
+  print(f"""setting checkpoint
+    load: {source.database}.{source.database} to {raw.database}.{raw.database}
+    path: {raw.checkpoint}
+  """)
+  config.set_checkpoint(source=source, destination=raw)
 
 
 # COMMAND ----------
@@ -124,11 +253,12 @@ pprint(raw.dict())
 
 # COMMAND ----------
 
-load(source, raw)
+load(param_process_id, source, raw)
 
 # COMMAND ----------
 
 load_hf(source, raw)
+
 
 # COMMAND ----------
 
