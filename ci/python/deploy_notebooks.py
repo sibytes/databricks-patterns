13a9d1edc990c7776b64e1a2a439db92922cddfa
Shaun Ryan <shaunryan@WorlordV.local>
2023-05-05 21:10:16 +0100

    simplifying notebook deployment

--- ci/python/deploy_notebooks.py
+++ ci/python/deploy_notebooks.py
@@ -1,26 +1,15 @@
 import os
 from autobricks import Workspace
 
-ROOT_DIR = os.getenv("ROOT_DIR")
-# WORKSPACE_ROOT = os.getenv("WORKSPACE_ROOT")
-# WORKSPACE_SUBDIRS:str = os.getenv("WORKSPACE_SUBDIRS")
-NOTEBOOK_DIR:str = os.getenv("NOTEBOOK_DIR")
+FROM_ROOT = os.getenv("FROM_ROOT")
+FROM_DIR = os.getenv("FROM_DIR")
+TO_WORKSPACE_DIR:str = os.getenv("TO_WORKSPACE_DIR")
 
-from_notebook_root = f"{ROOT_DIR}"
-source_dir = NOTEBOOK_DIR
-# target_dir = f"/{WORKSPACE_ROOT}"
-# sub_folders = [f"/{d.strip()}" for d in WORKSPACE_SUBDIRS.split(",")]
+from_path = f"{FROM_ROOT}/{FROM_DIR}"
+to_path = f"/{TO_WORKSPACE_DIR}"
 
-# for source_dir in sub_folders:
-
-    # this will iterate all dirs in the from_notebook_root
-    # if the root of the dir matches the source_dir then it
-    # will deploy the folder otherwise it will skip it.
-    # Deploy mode is PARENT it will copy the source dir 
-    # into the target dir in the worksapce
 Workspace.workspace_import_dir(
-    from_notebook_root=from_notebook_root,
-    source_dir=source_dir,
-    # target_dir=target_dir,
-    deploy_mode=Workspace.DeployMode.DEFAULT
+    from_path=from_path,
+    to_path=to_path,
+    # sub_dirs=["databricks","pipelines"]
 )
