74c175bfe5a86a3e8d4f7848536655d9c4f08be6
Shaun Ryan <shaunryan@WorlordV.local>
2023-06-05 10:09:30 +0100

    pipelining ad works dw

--- ad_works_dw/databricks/notebooks/clear_down.py
+++ ad_works_dw/databricks/notebooks/clear_down.py
@@ -8,16 +8,16 @@
 # DBTITLE 1,Clear Down the Data Lakehouse
 def clear_down():
   checkpoints = [
-    "/mnt/datalake/data/raw/raw_ad_works",
-    "/mnt/datalake/data/base/base_ad_works",
-    "/mnt/datalake/data/control/control_ad_works",
-    "/mnt/datalake/checkpoint/ad_works"
+    "/mnt/datalake/data/raw/raw_ad_works_dw",
+    "/mnt/datalake/data/base/base_ad_works_dw",
+    "/mnt/datalake/data/control/control_ad_works_dw",
+    "/mnt/datalake/checkpoint/ad_works_dw"
   ]
   for c in checkpoints:
     dbutils.fs.rm(c, True)
-  spark.sql("drop database if exists raw_ad_works CASCADE")
-  spark.sql("drop database if exists base_ad_works CASCADE")
-  spark.sql("drop database if exists control_ad_works CASCADE")
+  spark.sql("drop database if exists raw_ad_works_dw CASCADE")
+  spark.sql("drop database if exists base_ad_works_dw CASCADE")
+  spark.sql("drop database if exists control_ad_works_dw CASCADE")
 clear_down()
 
 # COMMAND ----------
