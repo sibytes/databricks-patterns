75d45df3023b9a1c08f04166a27bff3a0d2566de
Shaun Ryan <shaunryan@WorlordV.local>
2023-06-10 19:45:51 +0100

    dlt pipeline deployment

---
+++ ci/python/deploy_pipelines.py
@@ -0,0 +1,10 @@
+import os
+from autobricks import Pipeline
+
+ROOT_DIR = os.getenv("ROOT_DIR")
+SUB_DIR = os.getenv("SUB_DIR")
+
+
+path = os.path.join(ROOT_DIR, SUB_DIR)
+
+Pipeline.pipeline_import_jobs(path)
