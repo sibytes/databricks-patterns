959a0dc134da9e39fe0519681ed138ec6915237e
Shaun Ryan <shaunryan@WorlordV.local>
2023-06-16 21:26:19 +0100

    header footer base

---
+++ header_footer/sql/yetl_control_header_footer/base_audit.sql
@@ -0,0 +1,39 @@
+  CREATE TABLE IF NOT EXISTS `yetl_control_header_footer`.`base_audit`
+  (
+
+    `file_name` string,
+    source_database string,
+    source_table string,
+    `database` string,
+    `table` string,
+    total_count bigint,
+    valid_count bigint,
+    invalid_count bigint,
+    invalid_ratio double,
+    expected_row_count bigint,
+    warning_thresholds struct<
+      invalid_ratio:double,
+      invalid_rows:bigint,
+      max_rows:bigint,
+      min_rows:bigint
+    >,
+    exception_thresholds struct<
+      invalid_ratio:double,
+      invalid_rows:bigint,
+      max_rows:bigint,
+      min_rows:bigint
+    >,
+    file_path string,
+    file_size bigint,
+    file_modification_time timestamp,
+    _slice_date timestamp,
+    _process_id bigint,
+    _load_date timestamp
+  )
+  USING DELTA
+  LOCATION '{{location}}'
+  TBLPROPERTIES (
+    {{delta_properties}}
+  )
+
+  
